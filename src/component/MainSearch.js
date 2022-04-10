e59a41c6a081cdbed75a836173ef4c72af9d21dd
박유경 <51533341+codename-602@users.noreply.github.com>
2022-04-10 23:26:23 +0900

    [chore] 홈 css 수정
    예시 부분 줄 간격 추가 수정 필요함

--- src/component/MainSearch.js
+++ src/component/MainSearch.js
@@ -25,15 +25,39 @@ class MainSearch extends Component {
 class SearchOption extends Component {
     render() {
       return (
-        <div className="SelectSearchOption">
-          <div className="SearchOption">
-          <input type="checkbox" name="search-opt" checked
-          id="picture" value="picture" onClick={() => Home.checkOnlyOne('picture')}/> 장면검색
-          <input type="checkbox" name="search-opt" 
-          id="text" value="text" onClick={() => Home.checkOnlyOne('text')}/> 대사검색
+        <div className="SearchOption">
+          <div className="SelectOption">
+            <div className="option">
+              <input type="checkbox" class="search-opt" name="search-opt" checked
+              id="picture" value="picture" onClick={() => Home.checkOnlyOne('picture')}/>
+              <label>장면검색</label>
+            </div>
+
+            <div className="option">
+              <input type="checkbox" class="search-opt" name="search-opt" 
+              id="text" value="text" onClick={() => Home.checkOnlyOne('text')}/>
+              <label>대사검색</label>
+            </div>
           </div>
-          <SearchImgExample></SearchImgExample>
-          <SearchScriptExample></SearchScriptExample>
+
+          <div className="example">
+            <SearchImgExample></SearchImgExample>
+            <SearchScriptExample></SearchScriptExample>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  class SearchImgExample extends Component {
+    render() {
+      return (
+        <div className="SearchImgExample" id="search-image-ex">
+            <img id="image-search-ex" src={ ImgEx } width={"180rem"} height={"120rem"}/>
+            <span>
+                <br/>영화 '타짜'에 등장하는<br/>화투를 치는 장면입니다.<br/>
+                다음 예시와 같이<br/>여러분들의 기억 속에 있는<br/>영화 장면을 검색해 보세요!
+            </span>
         </div>
       );
     }
@@ -45,21 +69,7 @@ class SearchOption extends Component {
         <div className="SearchScriptExample" id="search-script-ex">
             <span>
                 <br/>'손모가지 날아가붕게'는 영화 '타짜'에 등장하는 명대사입니다.<br/>
-                다음 예시와 같이 여러분들의 기억 속에 있는 영화 대사를 검색해 보세요!
-            </span>
-        </div>
-      );
-    }
-  }
-
-  class SearchImgExample extends Component {
-    render() {
-      return (
-        <div className="SearchImgExample" id="search-image-ex">
-            <img id="image-search-ex" src={ ImgEx } width={"200px"} height={"150px"}/>
-            <span>
-                <br/>영화 '타짜'에 등장하는 화투를 치는 장면입니다.
-                다음 예시와 같이 여러분들의 기억 속에 있는 영화 장면을 검색해 보세요!
+                다음 예시와 같이 여러분들의 기억 속에 있는 영화 대사를 <br/>검색해 보세요!
             </span>
         </div>
       );
