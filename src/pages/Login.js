353bf6fbae5387a253bcf68ed69b1c1f1d5f2f6e
박유경 <51533341+codename-602@users.noreply.github.com>
2022-04-10 23:53:22 +0900

    [chore] 로그인 css 수정
    추후에 Link 태그 hover 적용할 것

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { Route, Link } from 'react-router-dom'
 import { SignUp } from '.';
 import Header from '../component/Header';
@@ -14,14 +14,14 @@ const Login = () => {
                 <div className="LoginArea">
                     <div className="InputArea">
                         <input type="text" placeholder=" ID"></input>
-                        <input type="text" placeholder=" PW"></input>
+                        <input type="password" placeholder=" PW"></input>
                     </div>
                     <button id="login-btn">로그인</button>
                 </div>
-                <div className="OptionArea">
-                    <Link to="/signup">회원가입</Link>
-                    <Link to="/">ID/PW 찾기</Link>
-                </div>
+                <ul className="OptionArea">
+                    <li><Link style={{textDecoration: 'none', color: 'white'}} id="signup-btn" to="/signup">회원가입</Link></li>
+                    <li><Link style={{textDecoration: 'none', color: 'white'}} id="find-btn" to="/">ID/PW 찾기</Link></li>
+                </ul>
                 
             </div>
         </div>
