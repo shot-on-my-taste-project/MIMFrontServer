4dbcd1480a504d4b4b7e8ab70914944b460ea562
박유경 <51533341+codename-602@users.noreply.github.com>
2022-04-11 00:06:12 +0900

    [chore] 회원가입 html 작성

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react'
 import '../styles/Home.css'
+import '../styles/SignUp.css'
 import Header from '../component/Header';
 import Logo from '../component/Logo';
 const SignUp = () => {
     return (
-        <div>
+        <div className="SignUpArea">
             <Header></Header>
             <Logo></Logo>
+            <div className="UserInfoArea">
+                <table>
+                    <tr>
+                        <th colspan="1">아이디</th>
+                        <td colspan="1">
+                            <input type="text" placeholder="아이디"/>
+                            <button>중복확인</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th colspan="1">비밀번호</th>
+                        <td colspan="1">
+                            <input type="password" placeholder="비밀번호"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th colspan="1"></th>
+                        <td colspan="1">
+                            <input type="password" placeholder="비밀번호 재입력"/>
+                            <button>일치확인</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th colspan="1">닉네임</th>
+                        <td colspan="1">
+                            <input type="text" placeholder="닉네임"/>
+                            <button>중복확인</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th colspan="1">프로필 사진</th>
+                        <td colspan="1">
+                            <input type="file" placeholder=""/>
+                        </td>
+                    </tr>
+                </table>
+                <button id="signup-btn">SIGN UP</button>
+            </div>
             
         </div>
     );
