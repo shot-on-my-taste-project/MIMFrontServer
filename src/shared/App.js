fd2d5516648b6ffa1b5bea570f80ee50dac0367c
박유경 <51533341+codename-602@users.noreply.github.com>
2022-04-11 22:31:21 +0900

    [chore] 부트스트랩 추가에 따른 자잘한 수정
    Home.css에 있던 공통 부분을 Common.css로 따로 뺌

--- src/shared/App.js
+++ src/shared/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Menu from '../component/Menu';
 import { Home, About, Login, SignUp } from '../pages'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from "react-bootstrap";
+import '../styles/Common.css'
+
 class App extends Component {
     render() {
         return (
