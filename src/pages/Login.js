eef88d92e7a230ee23b1bcedb85a45affea65a82
박유경 <51533341+codename-602@users.noreply.github.com>
2022-05-16 21:06:05 +0900

    [chore] 로그인, 회원가입 연동
    해야할 것
    1. 비밀번호 해싱
    2. 중복 확인
    3. 작업 완료 후 페이지 이동

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
-import React, { Component, useState } from 'react'
+import React, { Component, useState, useEffect } from 'react'
+import axios from 'axios';
 import { Route, Link } from 'react-router-dom'
 import {Button} from "react-bootstrap";
 import { SignUp } from '.';
@@ -6,6 +7,31 @@ import Header from '../component/Header';
 import Logo from '../component/Logo';
 import '../styles/Login.css'
 const Login = () => {
+    const [inputId, setInputId] = useState('')
+    const [inputPw, setInputPw] = useState('')
+
+    const handleInputId = (e) => {
+        setInputId(e.target.value)
+    }
+
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value)
+    }
+
+    const onClickLogin = () => {
+        axios.post('/login', {
+            "id": inputId,
+            "pw": inputPw,
+            "isRemoved": false,
+            "nickname": "",
+            "profilePath": "",
+            "role": "USER"
+          })
+          .then(res => console.log(res))
+          .catch();
+          console.log("click login")
+    }
+    
     return (
         <div className="LoginTotalArea">
             <Header></Header>
@@ -13,10 +39,10 @@ const Login = () => {
             <div className="LoginWrapper">
                 <div className="LoginArea">
                     <div className="InputArea">
-                        <input type="text" placeholder=" ID"></input>
-                        <input type="password" placeholder=" PW"></input>
+                        <input type="text" placeholder=" ID" name="id" value={inputId} onChange={handleInputId}></input>
+                        <input type="password" placeholder=" PW" name="pw" value={inputPw} onChange={handleInputPw}></input>
                     </div>
-                    <Button variant="danger" id="login-btn">로그인</Button>
+                    <Button variant="danger" id="login-btn" onClick={onClickLogin}>로그인</Button>
                 </div>
                 <ul className="OptionArea">
                     <li><Link style={{textDecoration: 'none', color: 'white'}} id="signup-btn" to="/signup">회원가입</Link></li>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
+import axios from 'axios';
 import {Button, Table} from "react-bootstrap";
 import '../styles/SignUp.css'
 import Header from '../component/Header';
 import Logo from '../component/Logo';
 const SignUp = () => {
+    const [inputId, setInputId] = useState('')
+    const [inputPw, setInputPw] = useState('')
+    const [inputNickName, setInputNickName] = useState('')
+
+    const handleInputId = (e) => {
+        setInputId(e.target.value)
+    }
+
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value)
+    }
+
+    const handleInputNickName = (e) => {
+        setInputNickName(e.target.value)
+    }
+
+    const onClickSignUp = () => {
+        axios.post('/sign-up', {
+            "id": inputId,
+            "pw": inputPw,
+            "isRemoved": false,
+            "nickname": inputNickName,
+            "profilePath": "",
+            "role": "USER"
+          })
+          .then(res => console.log(res))
+          .catch();
+          console.log("click signup")
+    }
+
     return (
         <div className="SignUpArea">
             <Header></Header>
@@ -13,14 +44,14 @@ const SignUp = () => {
                     <tr>
                         <th colspan="1">아이디</th>
                         <td colspan="1">
-                            <input class="userInfo" type="text" placeholder="아이디"/>
+                            <input class="userInfo" type="text" placeholder="아이디" name="id" value={inputId} onChange={handleInputId}/>
                             <Button variant="secondary" size="sm" className="subBtn">중복<br/>확인</Button>
                         </td>
                     </tr>
                     <tr>
                         <th colspan="1">비밀번호</th>
                         <td colspan="1">
-                            <input class="userInfo" type="password" placeholder="비밀번호"/>
+                            <input class="userInfo" type="password" placeholder="비밀번호" name="pw" value={inputPw} onChange={handleInputPw}/>
                         </td>
                     </tr>
                     <tr>
@@ -33,7 +64,7 @@ const SignUp = () => {
                     <tr>
                         <th colspan="1">닉네임</th>
                         <td colspan="1">
-                            <input class="userInfo" type="text" placeholder="닉네임"/>
+                            <input class="userInfo" type="text" placeholder="닉네임" name="nickName" value={inputNickName} onChange={handleInputNickName}/>
                             <Button variant="secondary" size="sm" className="subBtn">중복<br/>확인</Button>
                         </td>
                     </tr>
@@ -44,7 +75,7 @@ const SignUp = () => {
                         </td>
                     </tr>
                 </table>
-                <Button variant="danger" id="signup-btn">SIGN UP</Button>
+                <Button variant="danger" id="signup-btn" onClick={onClickSignUp}>SIGN UP</Button>
             </div>
             
         </div>
