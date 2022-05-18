befbfa16977f9cf2862f289b7cbd72559eee486e
박유경 <51533341+codename-602@users.noreply.github.com>
2022-05-18 13:41:52 +0900

    [chore] 커뮤니티 비율 수정
    [해야 할 것]
    1. 버튼 수정
    2. 페이지네이션 5페이지 이상 처리

--- src/pages/CommunityMovie.js
+++ src/pages/CommunityMovie.js
@@ -20,6 +20,28 @@ const CommunityMovie = () => {
           { id: 1671, title: "학교 온 지 1시간도 안됐는데", time: "13:27" },
           { id: 1670, title: "개발이 끝이 없네", time: "13:26" },
           { id: 1669, title: "ㅁㄴㅇㄻㄴㅇㄹ", time: "13:25" },
+          { id: 1668, title: "asdgfdsafd", time: "13:24" },{ id: 1678, title: "동작 그만 밑장빼기냐?", time: "13:33" },
+          { id: 1677, title: "묻고 트리플로 가", time: "13:32" },
+          { id: 1676, title: "2시부터 단관할 사람", time: "13:32" },
+          { id: 1675, title: "곧 사쿠라의 계절이네요", time: "13:30" },
+          { id: 1674, title: "싸늘하다 가슴에 비수가 날아와 꽂힌다", time: "13:29" },
+          { id: 1673, title: "집에 가고 싶다", time: "13:28" },
+          { id: 1672, title: "침대가 보고 싶다", time: "13:28" },
+          { id: 1671, title: "학교 온 지 1시간도 안됐는데", time: "13:27" },
+          { id: 1670, title: "개발이 끝이 없네", time: "13:26" },
+          { id: 1669, title: "ㅁㄴㅇㄻㄴㅇㄹ", time: "13:25" },
+          { id: 1668, title: "asdgfdsafd", time: "13:24" },
+          { id: 1678, title: "동작 그만 밑장빼기냐?", time: "13:33" },
+          { id: 1677, title: "묻고 트리플로 가", time: "13:32" },
+          { id: 1676, title: "2시부터 단관할 사람", time: "13:32" },
+          { id: 1675, title: "곧 사쿠라의 계절이네요", time: "13:30" },
+          { id: 1674, title: "싸늘하다 가슴에 비수가 날아와 꽂힌다", time: "13:29" },
+          { id: 1673, title: "집에 가고 싶다", time: "13:28" },
+          { id: 1672, title: "침대가 보고 싶다", time: "13:28" },
+          { id: 1671, title: "학교 온 지 1시간도 안됐는데", time: "13:27" },
+          { id: 1670, title: "개발이 끝이 없네", time: "13:26" },
+          { id: 1669, title: "ㅁㄴㅇㄻㄴㅇㄹ", time: "13:25" },
+          { id: 1668, title: "asdgfdsafd", time: "13:24" }
         ]
         return boards;
       }
@@ -41,46 +63,60 @@ const CommunityMovie = () => {
       if (count === 0) 
         return <p>글 정보가 없습니다.</p>;
     return (
-        <div>
+        <div className="CommunityMovieContainer">
             <Header></Header>
             <img className="ThumbImg" src={ Thumb } width={"100%"} height={"50%"}/>
            
             <div className="BoardInfo">
-                <h1>타짜</h1><h4>789명과 함께 이야기 중</h4>
-                <Button>신고</Button><Button>즐겨찾기</Button>
+                <div className="Text">
+                  <h1>타짜</h1><h4>789명과 함께 이야기 중</h4>
+                </div>
+                <div className="Btn">
+                  <Button>신고</Button><Button>즐겨찾기</Button>
+                </div>
+                
             </div>
             
-            <div className="SearchArea">
+            <div className="SubSearchArea">
                 <input type="text"/><Button>검색</Button><Button>글쓰기</Button>
             </div>
             
-
-            <table className="tablecss table">
-            <thead>
-                <tr>
-                <th colSpan={1}>번호</th>
-                <th colSpan={3}>제목</th>
-                <th colSpan={1}>작성시간</th>
+            <div className="TableWrapper">
+              <div className="TableContainer">
+                <table className="tablecss table">
+                  <colgroup>
+                    <col width="10%"/>
+                    <col width="80%"/>
+                    <col width="10%"/>
+                  </colgroup>
+                <thead>
+                    <tr>
+                    <th>번호</th>
+                    <th>제목</th>
+                    <th>작성시간</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {pagedBoards.map((board) => (
+                <tr key={board.id}>
+                  <td>{board.id}</td>
+                  <td>{board.title}</td>
+                  <td>{board.time}</td>
                 </tr>
-            </thead>
-            <tbody>
-                {pagedBoards.map((board) => (
-            <tr key={board.id}>
-              <td colSpan={1}>{board.id}</td>
-              <td colSpan={3}>{board.title}</td>
-              <td colSpan={1}>{board.time}</td>
-            </tr>
-          ))}
-            </tbody>
-            </table>
-  
-            <Pagenation
-        pageSize={pageSize}
-        itemsCount={count}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-      />
+              ))}
+                </tbody>
+                </table>
+              </div>
+              
+    
+              <Pagenation
+                pageSize={pageSize}
+                itemsCount={count}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+              />
             </div>
+          </div>
         
     );
 };
