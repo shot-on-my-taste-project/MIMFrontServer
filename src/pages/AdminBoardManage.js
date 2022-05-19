ca5c2188988b553ce7470d638a345693736e2c66
박유경 <51533341+codename-602@users.noreply.github.com>
2022-05-20 00:27:04 +0900

    [chore] 관리자 기능 탭 구현
    css 손 봐야 함

--- src/pages/AdminBoardManage.js
+++ src/pages/AdminBoardManage.js
@@ -1,10 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Header from '../component/AdminHeader';
+import { Button } from 'react-bootstrap'
+import '../styles/Admin.css'
+
+
 const AdminBoardManage = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const tabContArr = [
+        {
+            tabTitle: (
+                <li className={activeIndex===0 ? "is-active" : ""} onClick={()=>tabClickHandler(0)}>개설 요청</li>
+            ),
+            tabCont: (
+                <div>탭1 내용</div>
+            )
+        },
+        {
+            tabTitle: (
+                <li className={activeIndex===1 ? "is-active" : ""} onClick={()=>tabClickHandler(1)}>폐쇄 요청</li>
+            ),
+            tabCont: (
+                <div>탭2 내용</div>
+            )
+        }
+    ];
+
+    const tabClickHandler=(index)=>{
+        setActiveIndex(index);
+    }
+    
     return (
         <div>
             <Header></Header>
-            게시판 관리
+            <div className="BoardManageThumb">
+                <h1>게시판 관리</h1>
+                <div className="SubSearchArea">
+                    <input type="text"/><Button>검색</Button>
+                </div>
+            </div>
+            <ul className="tabs is-boxed">
+                {tabContArr.map((section, index)=>{
+                    return section.tabTitle
+                })}
+            </ul>
+            <div>
+                {tabContArr[activeIndex].tabCont}
+            </div>
         </div>
     );
 };
--- src/pages/AdminReportManage.js
+++ src/pages/AdminReportManage.js
@@ -1,10 +1,57 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Header from '../component/AdminHeader';
+import { Button } from 'react-bootstrap'
 const AdminReportManage = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const tabContArr = [
+        {
+            tabTitle: (
+                <li className={activeIndex===0 ? "is-active" : ""} onClick={()=>tabClickHandler(0)}>게시글 신고</li>
+            ),
+            tabCont: (
+                <div>탭1 내용</div>
+            )
+        },
+        {
+            tabTitle: (
+                <li className={activeIndex===1 ? "is-active" : ""} onClick={()=>tabClickHandler(1)}>댓글 신고</li>
+            ),
+            tabCont: (
+                <div>탭2 내용</div>
+            )
+        },
+        {
+            tabTitle: (
+                <li className={activeIndex===1 ? "is-active" : ""} onClick={()=>tabClickHandler(2)}>게시판 신고</li>
+            ),
+            tabCont: (
+                <div>탭3 내용</div>
+            )
+        }
+    ];
+
+    const tabClickHandler=(index)=>{
+        setActiveIndex(index);
+    }
+
     return (
         <div>
             <Header></Header>
-            신고 관리
+            <div className="BoardManageThumb">
+                <h1>신고 관리</h1>
+                <div className="SubSearchArea">
+                    <input type="text"/><Button>검색</Button>
+                </div>
+            </div>
+            <ul className="tabs is-boxed">
+                {tabContArr.map((section, index)=>{
+                    return section.tabTitle
+                })}
+            </ul>
+            <div>
+                {tabContArr[activeIndex].tabCont}
+            </div>
         </div>
     );
 };
