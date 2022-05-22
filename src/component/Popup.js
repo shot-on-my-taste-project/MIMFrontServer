9363dcafc37c5993adc8e15597c0ef646e3a1ed8
박유경 <51533341+codename-602@users.noreply.github.com>
2022-05-22 11:00:02 +0900

    [chore] 모달 삭제 및 팝업 재사용 가능하게 수정

--- src/component/Popup.js
+++ src/component/Popup.js
@@ -1,16 +1,35 @@
 import CloseIcon from '@mui/icons-material/Close'
+import { Button } from 'react-bootstrap';
+import React, { Component } from 'react';
 import '../styles/Popup.css'
-const Popup = (props) => {
-    return (
-        <div className="PopupBackGround">
-            <div className="Popup">
-                <div className="x" onClick={() => props.handlePopup()}>
-                    <CloseIcon />
-                </div>
-                신고 사유
-            </div>
-        </div>
-    )
-}
 
+class Popup extends Component {
+    render() {
+      // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
+      const { open, close, header } = this.props;
+  
+      return (
+          
+        <div className={open ? 'openPopup Popup' : ''}>
+          {open ? (
+            <section>
+              <header>
+                {header}
+                <div className="close" onClick={close}>
+                <CloseIcon />
+                </div>
+              </header>
+              <main>{this.props.children}</main>
+              <footer>
+                <Button className="close" variant="secondary" onClick={close}>
+                  닫기
+                </Button>
+              </footer>
+            </section>
+          ) : null}
+        </div>
+      );
+    }
+  }
+  
 export default Popup;
