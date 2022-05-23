85ae430857e3074d20a90674683fa79d7a6b15e6
박유경 <51533341+codename-602@users.noreply.github.com>
2022-05-23 15:28:56 +0900

    [chore] 회원 탈퇴 함수 작성

--- src/pages/MyPageUpdate.js
+++ src/pages/MyPageUpdate.js
@@ -1,18 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from 'react-bootstrap';
 import { Route, Link } from 'react-router-dom'
 import Header from '../component/Header';
 import profileImg from "../assets/profile.jpg"
 import '../styles/MyPage.css'
+import { getCookie } from '../utils/Cookie';
+import axios from 'axios';
 
 const MyPageUpdate = () => {
+    const [inputNickName, setInputNickName] = useState('')
+    let [inputOriginPw, setInputOriginPw] = useState('')
+    let [inputNewPw, setInputNewPw] = useState('')
+    const [inputProfilePath, setProfilePath] = useState('')
+    
+    const handleInputNickName = (e) => {
+        setInputNickName(e.target.value)
+    }
+    
+    const handleInputOriginPw = (e) => {
+        setInputOriginPw(e.target.value)
+    }
+
+    const handleInputNewPw = (e) => {
+        setInputNewPw(e.target.value)
+    }
+
+    const duplicateNickname = () => {
+        var nickNameInput = document.getElementById("nickName");
+        axios.get('/users/nick-name/' + inputNickName)
+          .then(res => {
+            if(res.data){
+                alert("중복된 닉네임입니다.")
+                nickNameInput.value = null;
+                nickNameInput.focus();
+              } else {
+                alert("사용 가능한 닉네임입니다.")
+              }
+          })
+          .catch(err => {
+
+          })
+    }
+
+
+    const samePassword = () => {
+        var firstInput = document.getElementById("first-pw");
+        var secondInput = document.getElementById("second-pw");
+        
+        if(firstInput.value === secondInput.value) {
+            alert("비밀번호가 일치합니다.")
+        } else {
+            alert("비밀번호가 일치하지 않습니다.")
+            secondInput.value = null
+            secondInput.focus();
+        }
+    }
+
+    const leaveMember = () => {
+        if(!window.confirm("탈퇴하시겠습니까?")) {
+            alert("탈퇴를 취소하였습니다.")
+        } else {
+            axios({
+                url: '/users/' + `${getCookie('user-id')}`,
+                method: 'delete',
+                headers: {
+                    "X-ACCESS-TOKEN": `${getCookie('user-id')}`,
+                    "X-REFRESH-TOKEN": localStorage.getItem("refresh-token")
+                }
+            })
+            alert("탈퇴되었습니다.")
+        }
+    }
+
     return ( 
         <div className="MyPageArea">
             <Header></Header>
             <div className="MainInfoUpdate">
-                <img src={ profileImg } width={ "150rem" }/>
+                <div className="profileArea">
+                    <img src={ profileImg } width={ "150rem" }/>
+                    <input type="file"/>
+                </div>
+
                 <input type="text"/>
-                <Button id="duplicate-check" variant="secondary">중복확인</Button>
+                <Button id="duplicate-check" variant="secondary" onClick={ duplicateNickname }>중복확인</Button>
             </div>
 
             <div className="InputWrapper">
@@ -20,26 +90,27 @@ const MyPageUpdate = () => {
                     <tr>
                         <th colspan="1">기존 비밀번호</th>
                         <td colspan="1">
-                            <input class="userInfo" type="text" placeholder="기존 비밀번호"/>                        </td>
+                            <input class="userInfo" type="text" placeholder="기존 비밀번호"/>
+                        </td>
                     </tr>
                     <tr>
                         <th colspan="1">새 비밀번호</th>
                         <td colspan="1">
-                            <input class="userInfo" type="password" placeholder="새 비밀번호"/>
+                            <input class="userInfo" type="password" id="first-pw" placeholder="새 비밀번호"/>
                         </td>
                     </tr>
                     <tr>
                         <th colspan="1">새 비밀번호<br/>재입력</th>
                         <td colspan="1">
-                            <input class="userInfo" type="password" placeholder="새 비밀번호 재입력"/>
-                            <Button id="same-check" variant="secondary" size="sm" className="subBtn">일치<br/>확인</Button>
+                            <input class="userInfo" type="password" id="second-pw" placeholder="새 비밀번호 재입력"/>
+                            <Button id="same-check" variant="secondary" size="sm" onClick={samePassword} className="subBtn">일치<br/>확인</Button>
                         </td>
                     </tr>
                 </table>
             </div>
             <div className="ButtonWrapper">
                 <Button variant="danger">수정</Button>
-                <Button variant="secondary">탈퇴</Button>
+                <Button variant="secondary" onClick={ leaveMember }>탈퇴</Button>
                 <Button variant="secondary">취소</Button>
             </div>
         </div>
