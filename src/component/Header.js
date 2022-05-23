bc2e4671c19303af6ebbfb65ac4242e5e30e76cc
박유경 <51533341+codename-602@users.noreply.github.com>
2022-05-23 14:28:47 +0900

    [chore] 로그인 여부에 따른 마이페이지 링크

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,16 +2,30 @@ import React, { Component } from 'react';
 import SideBar from "./SideBar";
 import MemberImg from "../assets/member.png"
 import { Link } from 'react-router-dom';
+import { getCookie } from '../utils/Cookie';
+import axios from 'axios';
+
 class Header extends Component {
     render() {
-        return (
-            <div className="TopBar">
-                <SideBar></SideBar>
-                <Link to="/mypage">
-                    <img id="member" src={ MemberImg } width={"40px"} height={"40px"}/>
-                </Link>
-            </div>
-        );
+        if(getCookie("access-token") != null) {
+            return (
+                <div className="TopBar">
+                    <SideBar></SideBar>
+                    <Link to="/mypage">
+                        <img id="member" src={ MemberImg } width={"40px"} height={"40px"}/>
+                    </Link>
+                </div>
+            );
+        } else {
+            return (
+                <div className="TopBar">
+                    <SideBar></SideBar>
+                    <Link to="/login">
+                        <img id="member" src={ MemberImg } width={"40px"} height={"40px"}/>
+                    </Link>
+                </div>
+            );
+        }
     };
 }
 
