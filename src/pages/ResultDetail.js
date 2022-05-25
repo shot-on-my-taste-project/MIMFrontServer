3ecf929dfee3aef9bb147a629c5a5e498abfc17a
박유경 <51533341+codename-602@users.noreply.github.com>
2022-05-25 17:12:19 +0900

    [chore] 영화 세부 정보 바로 안 뜨는 에러 해결

--- src/pages/ResultDetail.js
+++ src/pages/ResultDetail.js
@@ -8,49 +8,51 @@ import '../styles/Result.css'
 import axios from 'axios';
 
 const ResultDetail = ({ match }) => {
-    const [ movieInfo, setMovieInfo ] = useState([]);
-    
+    const movieId = match.params.movieId;
+    const linkToCommunity = '/community/movie/' + movieId;
+    const [ movieInfo, setMovieInfo ] = useState(null);
+
     useEffect(() => {
-        axios.get('/movies/' + match.params.movieId)
+        axios.get('/movies/' + movieId)
         .then(res => {
             setMovieInfo(res.data);
         })
         .catch(err => {
             console.log(err)
         })
-    }, [])
+    }, []);
 
-    console.log('무비데이터', movieInfo);
-
-    return ( 
-        <div className="ResultArea">
-            <Header></Header>
-            <div className="ContentWrapper">
-                <div className="MovieInfo">
-                    <img src={ ImgEx1 } width={"300rem"} height={"400rem"}/>
-                    <div className="TextInfo">
-                        <h1>{movieInfo.movieDto.title}</h1>
-                        <span>{movieInfo.movieDto.year}</span> | <span>{movieInfo.rating}</span> | <span>{movieInfo.movieDto.runningTime}분</span> |
-                        {movieInfo.genres.map((genre) => <span> {genre} </span>)}
-
-                        <p>{movieInfo.movieDto.synopsis}</p>
-                        <p>감독: {movieInfo.directors.map((director) => <span>{director} </span>)}</p>
-                        <p>주연: {movieInfo.actors.map((actor) => <span>{actor} </span>)}</p>
+    if(movieInfo) {
+        return ( 
+            <div className="ResultArea">
+                <Header></Header>
+                <div className="ContentWrapper">
+                    <div className="MovieInfo">
+                        <img src={ ImgEx1 } width={"300rem"} height={"400rem"}/>
+                        <div className="TextInfo">
+                            <h1>{movieInfo.movieDto.title}</h1>
+                            <span>{movieInfo.movieDto.year}</span> | <span>{movieInfo.rating}</span> | <span>{movieInfo.movieDto.runningTime}분</span> |
+                            {movieInfo.genres.map((genre) => <span> {genre} </span>)}
+    
+                            <p>{movieInfo.movieDto.synopsis}</p>
+                            <p>감독: {movieInfo.directors.map((director) => <span>{director} </span>)}</p>
+                            <p>주연: {movieInfo.actors.map((actor) => <span>{actor} </span>)}</p>
+                        </div>
                     </div>
-                </div>
-                
-                <div className="ShortCutCommunity">
-                    <h2>이 영화를 더 즐기고 싶다면?</h2>
-                    <Link to="/community/movie">
-                    <div id="slate">
-                    <img id="slate-open" src={ SlateImg1 } width={"80px"} height={"80px"}/>
-                    <img id="slate-close" src={ SlateImg2 } width={"80px"} height={"70px"}/>
+                    
+                    <div className="ShortCutCommunity">
+                        <h2>이 영화를 더 즐기고 싶다면?</h2>
+                        <Link to={ linkToCommunity }>
+                        <div id="slate">
+                        <img id="slate-open" src={ SlateImg1 } width={"80px"} height={"80px"}/>
+                        <img id="slate-close" src={ SlateImg2 } width={"80px"} height={"70px"}/>
+                        </div>
+                        </Link>
                     </div>
-                    </Link>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default ResultDetail;
