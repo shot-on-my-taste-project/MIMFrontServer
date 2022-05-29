5bad39e512de747a95d53f47179f35eaf5ac4393
박유경 <51533341+codename-602@users.noreply.github.com>
2022-05-29 12:26:48 +0900

    [chore] 통합자유게시판 글 작성

--- src/component/WriteBoard.js
+++ src/component/WriteBoard.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom'
+import React from 'react';
 import { Button } from 'react-bootstrap'
-import * as Home from '../services/home.js'
 
-class WriteBoard extends Component {
-    render() {
-        return (
-            <div className="WriteArea">
-                <div className="WriteWrapper">
-                    <table>
-                        <tr>
-                            <th>제목</th>
-                            <td>
-                                <input id="board-title" type="text"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>내용</th>
-                            <td>
-                                <textarea id="board-content"/>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div className="ButtonWrapper">
-                    <Button variant="danger">등록</Button>
-                    <Button variant="secondary">취소</Button>
-                </div>
+const WriteBoard = (props) => {
+    const { titleHandler, contentHandler, enrollEvent, cancelEvent } = props;
+    
+    return (
+        <div className="WriteArea">
+            <div className="WriteWrapper">
+                <table>
+                    <tr>
+                        <th>제목</th>
+                        <td>
+                            <input id="board-title" type="text" onChange={titleHandler}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>내용</th>
+                        <td>
+                            <textarea id="board-content" onChange={contentHandler}/>
+                        </td>
+                    </tr>
+                </table>
             </div>
-        );
-    }
+            <div className="ButtonWrapper">
+                <Button onClick={ enrollEvent } variant="danger">등록</Button>
+                <Button onClick={ cancelEvent } variant="secondary">취소</Button>
+            </div>
+        </div>
+);
+    
 }
 
 export default WriteBoard;
--- src/pages/FreeBoardWrite.js
+++ src/pages/FreeBoardWrite.js
@@ -1,15 +1,44 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Header from '../component/Header';
 import WriteBoard from '../component/WriteBoard';
 import '../styles/Community.css'
+import Api from '../utils/api/communityAPI';
+
 const FreeBoardWrite = () => {
+    const [inputTitle, setInputTitle] = useState('')
+    let [inputContent, setInputContent] = useState('')
+
+    const inputTitleHandler = (e) => {
+        setInputTitle(e.target.value)
+    }
+
+    const inputContentHandler = (e) => {
+        setInputContent(e.target.value)
+    }
+
+    const postData = {
+        "title": inputTitle,
+        "content": inputContent
+    }
+
+    const enrollEvent = async() => {await Api.writeFreeBoardPost(postData)}
+
+    const cancelEvent = () => {
+        window.location.href="/community/free"
+    }
+
     return (
         <div>
             <Header></Header>
             <div className="FreeBoardThumb">
                 <h1>자유 게시판</h1>
             </div>
-            <WriteBoard></WriteBoard>
+            <WriteBoard
+            titleHandler={inputTitleHandler}
+            contentHandler={inputContentHandler} 
+            enrollEvent={enrollEvent}
+            cancelEvent={cancelEvent}
+            ></WriteBoard>
         </div>
     );
 };
