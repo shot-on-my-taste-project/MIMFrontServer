c1b425e54d14769750d4d80e5fc73cd21d986545
박유경 <51533341+codename-602@users.noreply.github.com>
2022-05-31 17:22:42 +0900

    [chore] 오류 해결
    페이지 바로 뜨지 않는 오류

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,6 +6,9 @@ import { getCookie } from '../utils/Cookie';
 
 class Header extends Component {
     render() {
+        const goToMypage = () => {
+            window.location.href = "/mypage"
+        }
         //포스터 url 가져오기
         const getPoster = (userId) => `http://fhdufhdu.iptime.org:8081/users/${userId}/profile`;
         if(getCookie("access-token") != null) {
@@ -13,7 +16,7 @@ class Header extends Component {
                 <div className="TopBar">
                     <SideBar></SideBar>
                     <Link to="/mypage">
-                        <img id="member" src={ getPoster(getCookie('user-id')) } width={"40px"} height={"40px"}/>
+                        <img onClick={goToMypage} id="member" src={ getPoster(getCookie('user-id')) } width={"40px"} height={"40px"}/>
                     </Link>
                 </div>
             );
